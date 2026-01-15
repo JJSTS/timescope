09e9bc91da7b63504878c431e5923bba90a93718
2daw <juanjesustrejosalinas06@gmail.com>
2026-01-15 18:59:12 +0100

    Commit inicial

---
+++ src/main/java/es/timescope/TimescopeApplication.java
@@ -0,0 +1,13 @@
+package es.timescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimescopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimescopeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/timescope/TimescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.timescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimescopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
