cc13ed1e4f27b7df4a0a3b30829b03014393cfe1
JJSTS <juanjesustrejosalinas06@gmail.com>
2026-04-26 18:41:01 +0200

    Elaboracion de Notificacion - No completo

---
+++ src/main/java/es/timescope/rest/Notificacion/models/Tipo.java
@@ -0,0 +1,9 @@
+package es.timescope.rest.Notificacion.models;
+
+public enum Tipo {
+    SOLICITUD_RECIBIDA,
+    SOLICITUD_ACEPTADA,
+    SOLICITUD_RECHAZADA,
+    TAREA_ASIGNADA,
+    EQUIPO_UNIDO
+}
