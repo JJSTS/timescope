51449923754bef2fed2604bf4fde4e1b7c8de0f4
JJSTS <juanjesustrejosalinas06@gmail.com>
2026-04-27 20:45:23 +0200

    Solucion de errores de Solicitud

--- src/main/java/es/timescope/rest/Usuarios/models/Usuario.java
+++ src/main/java/es/timescope/rest/Usuarios/models/Usuario.java
@@ -7,6 +7,9 @@ import es.timescope.rest.Proyectos.models.Proyecto;
 import es.timescope.rest.Tareas.models.Tarea;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
@@ -22,7 +25,7 @@ import java.util.stream.Collectors;
 @Getter
 @Setter
 @Table(name = "USUARIOS")
-public class Usuario {
+public class Usuario implements UserDetails {
 //public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,9 +46,6 @@ public class Usuario {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
-    private Long tiempoProyecto;
-
     @ElementCollection(fetch = FetchType.EAGER)
     @Enumerated(EnumType.STRING)
     @CollectionTable(
@@ -59,12 +59,12 @@ public class Usuario {
     @Builder.Default
     private Boolean isDeleted = false;
 
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return roles.stream()
-//                .map(role -> new SimpleGrantedAuthority("ROLES_" + role.name()))
-//                .collect(Collectors.toSet());
-//    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLES_" + role.name()))
+                .collect(Collectors.toSet());
+    }
 
     @OneToMany(mappedBy = "usuario")
     @JsonIgnoreProperties("usuario")
@@ -81,29 +81,28 @@ public class Usuario {
     @JsonIgnore
     private Organizacion organizacion;
 
-    //Spring Security
-//    @Override
-//    public String getUsername() {
-//        return "";
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return UserDetails.super.isAccountNonExpired();
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return UserDetails.super.isAccountNonLocked();
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return UserDetails.super.isCredentialsNonExpired();
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return UserDetails.super.isEnabled();
-//    }
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
