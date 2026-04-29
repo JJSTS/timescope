43b1f84a9a8f1b6817e1cfbd288effa532c7665a
Carlos <carlos.casado10@educa.madrid.org>
2026-04-29 22:46:30 +0200

    Corrigo el ultimo merge

--- src/main/java/es/timescope/rest/Proyectos/services/ProyectoServicesImpl.java
+++ src/main/java/es/timescope/rest/Proyectos/services/ProyectoServicesImpl.java
@@ -61,9 +61,10 @@ public class ProyectoServicesImpl implements ProyectoServices {
     }
 
     @Override
-    public Page<Proyecto> findByEstado(Estado estado, Pageable pageable) {
+    public Page<ProyectoResponseDto> findByEstado(Estado estado, Pageable pageable) {
         log.info("Buscando proyectos por estado: {}", estado);
-        return proyectosRepository.findByEstado(estado, pageable);
+        return proyectosRepository.findByEstado(estado, pageable).
+                map(proyectoMapper::toProyectoResponseDto);
     }
 
     @Override
